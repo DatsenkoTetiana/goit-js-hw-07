8d618c1855d918a4b654267a9ffcf2ddbf6a0bb4
tetianadac <datsenkotetiana027@gmail.com>
2022-10-29 18:19:07 +0300

    two tasks

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+const boxGallery = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+
+boxGallery.insertAdjacentHTML("beforeend", markup);
+
+function createMarkup(items) {
+  return  items
+  .map(item => { 
+    return `
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  `;
+  })
+  .join(" ");
+};
+new SimpleLightbox(`.gallery a`, {
+    captionsData: `alt`,
+    captionsDelay: 250,
+});
 
 console.log(galleryItems);
